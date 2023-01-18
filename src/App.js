88058c34f808b81f006706878f058c203eff5c74
Tomáš Zeleňák <108126659+Tom4sko@users.noreply.github.com>
2023-01-18 14:29:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
   return (
     <div className="all-questions">
         <h1>Predkontácie</h1>
-        <p>Táto stránku slúži ako opakovanie na maturitnú skúšku. Ak ma chceš podporiť <a href="https://ko-fi.com/tom4sko" target="_blanc" className="link">klikni sem.</a></p>
+        <p>Táto stránka slúži ako opakovanie na maturitnú skúšku. Ak ma chceš podporiť <a href="https://ko-fi.com/tom4sko" target="_blanc" className="link">klikni sem.</a></p>
       <div className="all-questions-targets">
       {
         randomNum(5).map( (numberLength) => {
