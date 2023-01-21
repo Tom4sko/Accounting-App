7f6f795c350f3b0f1cf2277c0819e40d9bf388eb
Tomáš Zeleňák <108126659+Tom4sko@users.noreply.github.com>
2023-01-21 15:09:24 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import data from "./data"
 import Question from "./components/Question"
+import { BiRefresh } from "react-icons/bi"
+import { FaGithub, FaInstagram, FaCoffee } from "react-icons/fa";
 
 const App = () => {
 
@@ -19,8 +21,11 @@ const App = () => {
 
   return (
     <div className="all-questions">
+      <div className="header">
         <h1>Predkontácie</h1>
-        <p>Táto stránka slúži ako opakovanie na maturitnú skúšku. Ak ma chceš podporiť <a href="https://ko-fi.com/tom4sko" target="_blanc" className="link">klikni sem.</a></p>
+        <p>Táto stránku slúži ako opakovanie na maturitnú skúšku. Pre pridanie predkontácii ma kontaktujte.</p>
+      </div>
+
       <div className="all-questions-targets">
       {
         randomNum(5).map( (numberLength) => {
@@ -33,7 +38,13 @@ const App = () => {
       </div>
       
       <div className="buttons">
-        <button type="button" className="reset-btn" onClick={() => window.location.reload(true)}>Refresh</button>
+        <button type="button" className="reset-btn" onClick={() => window.location.reload(true)}> <BiRefresh className="reload-icon"/> Reload</button>
+      </div>
+
+      <div className="contact">
+        <a href="https://github.com/Tom4sko" target="_blank" rel="noopener noreferrer"> <FaGithub /> </a>
+        <a href="https://www.instagram.com/z.tomino/" target="_blank" rel="noopener noreferrer"> <FaInstagram /> </a>
+        <a href="https://ko-fi.com/tom4sko" target="_blank" rel="noopener noreferrer"> <FaCoffee /> </a>
       </div>
     </div>
   )
